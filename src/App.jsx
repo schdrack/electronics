1f6fd585d9fe80b3bd1d841483e735d12bfaa645
schadrack <muragijimanaschadrack183@gmail.com>
2025-06-06 23:03:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ const products = [
   { id: 2, name: 'Laptop Pro', price: 899.99, category: 'Laptops', rating: 4.8, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd7zrdTLnrh2Jdpng7oYD1PAASjAPDP4UFDw&s', description: 'High-performance laptop for professionals' },
   { id: 3, name: 'Wireless Earbuds', price: 79.99, category: 'Accessories', rating: 4.2, image: 'https://i5.walmartimages.com/seo/Wireless-Earbuds-Bluetooth-Headset-Microphone-Touch-Control-IPX7-Waterproof-High-Fidelity-Stereo-Earphones-Sports-Work-Compatible-iPhone-13-Pro-Max-X_ea3905c6-a23d-435d-952f-81b510607ac9.c223f55feb63d91007cc547d80932ba8.jpeg', description: 'Crystal clear sound with noise cancellation' },
   { id: 4, name: 'Smart Watch', price: 199.99, category: 'Wearables', rating: 4.3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTplNXkitXTcRw3Nxr_lv2WY05wd_REBnENDw&s', description: 'Track your fitness and stay connected' },
-  { id: 5, name: 'Tablet Mini', price: 249.99, category: 'Tablets', rating: 4.0, image: 'src/images/tablet.jpg', description: 'Compact tablet for entertainment on the go' },
-  { id: 6, name: 'Bluetooth Speaker', price: 59.99, category: 'Accessories', rating: 4.1, image: 'src/images/speaker.jpg', description: 'Portable speaker with rich bass' },
+  { id: 5, name: 'Tablet Mini', price: 249.99, category: 'Tablets', rating: 4.0, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8mzpG5gEZnFPMw3Vg5hhljP6w7AJGNdrOWA&s', description: 'Compact tablet for entertainment on the go' },
+  { id: 6, name: 'Bluetooth Speaker', price: 59.99, category: 'Accessories', rating: 4.1, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRODXxX4H_aHEAEYLo6f36xXAWgQ2yUIQiNBA&s', description: 'Portable speaker with rich bass' },
 ];
 
 const App = () => {
