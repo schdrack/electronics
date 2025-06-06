8eac963f608354a46700bff2a773c6c744c259a6
schadrack <muragijimanaschadrack183@gmail.com>
2025-06-06 23:14:31 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
         <div className="w-full md:w-1/3 flex flex-col items-center">
           <div className="relative mb-6 group">
             <img 
-              src="https://photos.fife.usercontent.google.com/pw/AP1GczOmObjXxKwhCgVq1dIO-OGsrPpcuaH6WG0xM5ASnd2huWHNWOSLqzVT=w963-h645-s-no-gm?authuser=0" // Replace with your actual image path
+              src="https://lh3.googleusercontent.com/a/ACg8ocKbPoCM1oK2aQ-X9iDRRo8SPSI_6DKoqfdtbrZYcsYDPT3WMFwV=s96-c-rg-br100" // Replace with your actual image path
               alt="Mura Infinity Tech"
               className="w-64 h-64 rounded-full object-cover border-4 border-blue-500 shadow-lg transform group-hover:scale-105 transition-transform duration-300"
             />
